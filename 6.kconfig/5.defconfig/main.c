55f70e2d7f9f30f33cbe6c02be5c1e4f2b855077
mz8023yt <mz8023yt@163.com>
2022-02-08 10:51:12 +0800

    [FEAT]完善配置编译逻辑2

--- 6.kconfig/5.defconfig/main.c
+++ 6.kconfig/5.defconfig/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include "include/linux/autoconf.h"
+
+extern void a_print(void);
+extern void a1_print(void);
+extern void b_print(void);
 
 void main_print(void)
 {
@@ -8,5 +13,18 @@ void main_print(void)
 int main()
 {
     main_print();
+
+#if (defined CONFIG_FUNC_A)
+    a_print();
+#endif
+
+#if (defined CONFIG_FUNC_A1)
+    a1_print();
+#endif
+
+#if (defined CONFIG_MENU_FUNC_B)
+    b_print();
+#endif
+
     return 0;
 }
